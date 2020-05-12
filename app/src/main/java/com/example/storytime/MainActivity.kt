687960fb5e4612df9611d86a122f7631ec7213e3
cleancode2k19 <49688548+cleancode2k19@users.noreply.github.com>
2020-05-12 19:29:47 +1000

    registration done

--- app/src/main/java/com/example/storytime/MainActivity.kt
+++ app/src/main/java/com/example/storytime/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.storytime
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,13 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val context = this;
-        btnLogin.run {
+        btnSignup.run {
             ;
-            btnLogin.setOnClickListener({
+            btnSignup.setOnClickListener({
                 if(readerEmail.text.toString().length>0 && readerPassword.text.toString().length>0){
-                    var reader = Reader("Bingo",readerEmail.text.toString(),readerPassword.text.toString())
-                    var db = DBHandler(context)
-                    db.insertData(reader)
+                    if(readerPassword.text.toString()==readerRepeatPassword.text.toString() && accept.isChecked) {
+                        var reader = Reader(
+                            "Bingo",
+                            readerEmail.text.toString(),
+                            readerPassword.text.toString()
+                        )
+                        var db = DBHandler(context)
+                        db.insertData(reader)
+                    } else{
+                        Toast.makeText(context,"Please fill corect password or accept the license aggrement",Toast.LENGTH_SHORT).show()
+                    }
                 } else {
                     Toast.makeText(context,"Please fill corect input",Toast.LENGTH_SHORT).show()
                 }
